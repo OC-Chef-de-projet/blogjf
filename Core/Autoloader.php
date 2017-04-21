ac7d8e8adf4908e141f93a8f53a910b298bc600f
Pierre-Sylvain AUGEREAU <ps.augereau@gmail.com>
2017-04-21 15:57:06 +0200

    Correction visibilité méthodes

--- Core/Autoloader.php
+++ Core/Autoloader.php
@@ -2,10 +2,10 @@
 namespace Core;
 
 class Autoloader {
-	static function register(){
+	public static function register(){
 		spl_autoload_register(array(__CLASS__,'autoload'));
 	}
-	static function autoload($class){	
+	public static function autoload($class){	
 		$class = preg_replace('#\\\#',DS,$class);		
 		require ROOT.'/'.$class.'.php';
 	}
